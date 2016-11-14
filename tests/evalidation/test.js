e883ad9312b5aa131b35a41e5c80f0936f0e89b4
Juan Cruz Viotti <jviotti@openmailbox.org>
2016-11-14 15:05:34 +0000

    Refactor test suite
    - Organise by image type
    - Remove coupling on images between different test suites
    
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

---
+++ tests/evalidation/test.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const m = require('mochainon');
+const path = require('path');
+const fs = require('fs');
+const Bluebird = require('bluebird');
+const imageWrite = require('../../lib');
+
+module.exports = [
+
+  {
+    name: 'should throw EVALIDATION is check fails',
+    data: {
+      input: path.join(__dirname, 'input'),
+      mock: path.join(__dirname, 'mock'),
+      output: path.join(__dirname, 'output')
+    },
+    case: (data) => {
+      const stream = fs.createReadStream(data.input);
+      const mockStream = fs.createReadStream(data.mock);
+
+      const createReadStreamStub = m.sinon.stub(fs, 'createReadStream');
+      createReadStreamStub.returns(mockStream);
+
+      return new Bluebird((resolve, reject) => {
+        const imageSize = fs.statSync(data.input).size;
+
+        const writer = imageWrite.write({
+          fd: fs.openSync(data.output, 'rs+'),
+          device: data.output,
+          size: imageSize * 1.2
+        }, {
+          stream: stream,
+          size: imageSize
+        }, {
+          check: true
+        });
+
+        writer.on('error', reject);
+        writer.on('done', resolve);
+
+      // Ensure we don't get false positives if the `.catch()`
+      // block is never called because validation passed.
+      }).then(() => {
+        throw new Error('Validation Passed');
+
+      }).catch((error) => {
+        m.chai.expect(error.code).to.equal('EVALIDATION');
+      }).finally(createReadStreamStub.restore);
+    }
+  }
+
+];
---
+++ tests/not-large-enough/test.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const m = require('mochainon');
+const path = require('path');
+const fs = require('fs');
+const Bluebird = require('bluebird');
+const imageWrite = require('../../lib');
+
+module.exports = [
+
+  {
+    name: 'should be rejected if the drive is not large enough',
+    data: {
+      input: path.join(__dirname, 'input'),
+      output: path.join(__dirname, 'output')
+    },
+    case: (data) => {
+      return new Bluebird((resolve, reject) => {
+        const writer = imageWrite.write({
+          fd: fs.openSync(data.output, 'rs+'),
+          device: data.output,
+          size: fs.statSync(data.output).size
+        }, {
+          stream: fs.createReadStream(data.input),
+          size: fs.statSync(data.input).size
+        });
+
+        writer.on('error', reject);
+        writer.on('done', resolve);
+      }).catch((error) => {
+        m.chai.expect(error).to.be.an.instanceof(Error);
+        m.chai.expect(error.code).to.equal('ENOSPC');
+        m.chai.expect(error.message).to.equal('Not enough space on the drive');
+      });
+    }
+  }
+
+];
