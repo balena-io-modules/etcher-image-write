049bf9b0a4efec00f149a890e6eefcc92aacf40f
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-03-01 19:17:32 +0100

    chore(versionist): Add changelog template

--- versionist.conf.js
+++ versionist.conf.js
@@ -1,19 +1,24 @@
 'use strict';
 
+var fs = require('fs');
+
 module.exports = {
   subjectParser: 'angular',
   editChangelog: true,
   editVersion: false,
+  template: fs.readFileSync( __dirname + '/doc/CHANGELOG.hbs', 'utf8' ),
   addEntryToChangelog: {
     preset: 'prepend',
     fromLine: 5
   },
   getIncrementLevelFromCommit: (commit) => {
-    if( /(feat)/i.test( commit.subject.type ) )
-      return 'minor'
+    if( commit.subject.type == 'feat' )
+      return 'minor';
     return 'patch';
   },
   transformTemplateData: (data) => {
+
+    console.log( data )
 
     data.features = data.commits.filter((commit) => {
       return commit.subject.type === 'feat';
